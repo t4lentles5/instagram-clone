c315e5ff7cbd390b91ccd1a321e934492bdff700
Omar Obregon <oobregon544@gmail.com>
2025-02-25 20:45:17 -0500

    Wrap application in ThemeProvider for dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import './globals.css';
+import { ThemeProvider } from '../components/ui/ThemeProvider';
 
 export const metadata: Metadata = {
   title: 'Instagram',
@@ -22,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
