be2d04d2a4e4974231e91fbe97557994558cb6d4
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-01 18:12:11 -0500

    Update ProfileUserPage to display dynamic username from params

--- src/app/(home)/[username]/page.tsx
+++ src/app/(home)/[username]/page.tsx
@@ -1,7 +1,13 @@
-export default function ProfileUserPage() {
+interface Props {
+  params: Promise<{ username: string }>;
+}
+
+export default async function ProfileUserPage({ params }: Props) {
+  const { username } = await params;
+
   return (
     <>
-      <h1>ProfileUserPage</h1>
+      <h1>{username}</h1>
     </>
   );
 }
