7a92a1d4996f527785996c40eb0bdf90dc7aa1fa
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-01 18:31:05 -0500

    Add LoginPage component and imageUrls data structure

---
+++ src/assets/imagesUrls.ts
@@ -0,0 +1,14 @@
+export const imageUrls = [
+  {
+    image_background:
+      'https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk',
+    image_1:
+      'https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www',
+    image_2:
+      'https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot3.png?__d=www',
+    image_3:
+      'https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot4.png?__d=www',
+    image_4:
+      'https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot2.png?__d=www',
+  },
+];
