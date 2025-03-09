613b4d34b07c7a79f36b050ed42cf75d715e846e
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-09 17:08:34 -0500

    Implement image carousel in LoginPage for dynamic image display

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,41 @@
+'use client';
+
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+const images = [
+  '/login/screenshot1.png',
+  '/login/screenshot2.png',
+  '/login/screenshot3.png',
+  '/login/screenshot4.png',
+];
 
 export default function LoginPage() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <main className='flex justify-center w-full h-full p-8'>
       <div className='bg-[url("/login/home-phones.png")] bg-no-repeat bg-[-46px_0] mr-8 bg-auto w-[380px] relative'>
-        <img
-          src='/login/screenshot1.png'
-          alt='img'
-          width={250}
-          className='absolute top-6 left-[110px]'
-        />
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt='img'
+            style={{
+              opacity: index === currentImageIndex ? 1 : 0,
+            }}
+            width={250}
+            className='absolute top-6 left-[110px] transition-opacity duration-300'
+          />
+        ))}
       </div>
       <div className='flex flex-col w-[350px] gap-3 p-3'>
         <div className='flex flex-col w-full gap-3 p-10 border border-separator'>
