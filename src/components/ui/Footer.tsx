ec188b716a5ba2debbe15b45382ce2fd9e30496e
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-10 20:52:01 -0500

    Update footer text to reflect cloning attribution

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <>
       <footer className='text-center text-[#a8a8a8] text-sm mt-5'>
-        Made with 💙 by{' '}
+        Cloned with 💙 by{' '}
         <Link
           href='https://github.com/Obrn544'
           target='_blank'
