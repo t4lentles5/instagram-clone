09164bda1c8b6c661c8b9331df03fb1d84d18816
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-23 17:14:55 -0500

    Improve error handling in getAuthenticatedUser function for better token validation and user redirection

--- src/actions/auth/get-authenticate-user.ts
+++ src/actions/auth/get-authenticate-user.ts
@@ -6,32 +6,44 @@ import getToken from '@/utils/cookies';
 import { redirect } from 'next/navigation';
 
 export const getAuthenticatedUser = async () => {
-  const token = await getToken();
+  try {
+    const token = await getToken();
 
-  if (!token) {
-    redirect('/auth/login');
-  }
-
-  const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-
-  if (typeof decoded === 'object' && 'id' in decoded) {
-    const user = await prisma.user.findUnique({
-      where: { id: decoded.id },
-      select: {
-        id: true,
-        email: true,
-        fullname: true,
-        username: true,
-        // profile_photo: true,
-      },
-    });
-
-    if (!user) {
-      throw new Error('User not found');
+    if (!token) {
+      redirect('/auth/login');
     }
 
-    return user;
-  }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
 
-  throw new Error('Invalid token');
+    if (typeof decoded === 'object' && 'id' in decoded) {
+      const user = await prisma.user.findUnique({
+        where: { id: decoded.id },
+        select: {
+          id: true,
+          email: true,
+          fullname: true,
+          username: true,
+          // profile_photo: true,
+        },
+      });
+
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      return user;
+    }
+
+    throw new Error('Invalid token');
+  } catch (error) {
+    console.error('Error validating token:', error);
+
+    if (error instanceof jwt.TokenExpiredError) {
+      redirect('/auth/login');
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      redirect('/auth/login');
+    } else {
+      throw error;
+    }
+  }
 };
