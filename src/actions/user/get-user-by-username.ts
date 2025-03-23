a1316dcbd2d63f2a4a2c6a968b85b98112671c35
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-22 21:21:39 -0500

    Add getUserByUsername function to retrieve user by username and handle not found cases

---
+++ src/actions/user/get-user-by-username.ts
@@ -0,0 +1,14 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+import { notFound } from 'next/navigation';
+
+export const getUserByUsername = async (username: string) => {
+  const user = await prisma.user.findUnique({ where: { username } });
+
+  if (!user) {
+    notFound();
+  }
+
+  return user;
+};
