55d4dd90143b4970d2dd07a3c5541ff4aa87a11c
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-22 21:21:34 -0500

    Refactor getAuthenticatedUser function to improve error handling and streamline token validation

--- src/actions/auth/get-authenticate-user.ts
+++ src/actions/auth/get-authenticate-user.ts
@@ -6,37 +6,32 @@ import getToken from '@/utils/cookies';
 import { redirect } from 'next/navigation';
 
 export const getAuthenticatedUser = async () => {
-  try {
-    const token = await getToken();
+  const token = await getToken();
 
-    if (!token) {
-      redirect('/auth/login');
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-
-    if (typeof decoded === 'object' && 'id' in decoded) {
-      const user = await prisma.user.findUnique({
-        where: { id: decoded.id },
-        select: {
-          id: true,
-          email: true,
-          fullname: true,
-          username: true,
-          // profile_photo: true,
-        },
-      });
-
-      if (!user) {
-        return null;
-      }
-
-      return user;
-    }
-
-    return null;
-  } catch (error) {
-    console.error('Error validating token:', error);
-    return null;
+  if (!token) {
+    redirect('/auth/login');
   }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+  if (typeof decoded === 'object' && 'id' in decoded) {
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+      select: {
+        id: true,
+        email: true,
+        fullname: true,
+        username: true,
+        // profile_photo: true,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
+  }
+
+  throw new Error('Invalid token');
 };
