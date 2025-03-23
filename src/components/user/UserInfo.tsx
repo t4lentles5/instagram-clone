03d418e3f957b8ede7084992c9a8db51fee9711d
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-23 18:45:44 -0500

    Refactor ProfileUserPage layout and enhance UserInfo component with navigation links

--- src/components/user/UserInfo.tsx
+++ src/components/user/UserInfo.tsx
@@ -11,7 +11,7 @@ interface Props {
 export const UserInfo = ({ user }: Props) => {
   return (
     <>
-      <main className='flex items-center w-full border-b pb-10 lg:w-[935px] border-separator'>
+      <main className='flex items-center w-full h-full pb-10 border-b border-separator'>
         <ProfilePhoto user={user} />
 
         <div className='flex flex-col w-full h-full gap-5 ml-5'>
@@ -46,6 +46,16 @@ export const UserInfo = ({ user }: Props) => {
           </div>
         </div>
       </main>
+
+      <div className='flex items-center w-full gap-10 p-3 text-xs border-b sm:hidden justify-evenly border-separator'>
+        <p className='text-center text-text'>0 posts</p>
+        <button className='px-2 py-1 font-bold rounded-lg text-text '>
+          0 Followers
+        </button>
+        <button className='px-2 py-1 font-bold rounded-lg text-text '>
+          0 Following
+        </button>
+      </div>
     </>
   );
 };
