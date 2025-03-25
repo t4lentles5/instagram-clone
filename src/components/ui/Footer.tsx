43f42cc49a14f32c0a57a4794e45fcc6fd4881f9
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-25 16:48:07 -0500

    Update Footer component to change font size from small to extra small for improved readability

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export const Footer = () => {
   return (
     <>
-      <footer className='p-5 text-sm text-center text-foregroundSecondary'>
+      <footer className='p-5 text-xs text-center text-foregroundSecondary'>
         Cloned with 💙 by{' '}
         <Link
           href='https://github.com/Obrn544'
