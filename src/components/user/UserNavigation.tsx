c41086ef3a3846e043fb893762e2466dbb62d8e6
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-30 11:56:25 -0500

    Add new icon components for Posts, Reels, and Tagged, and update UserNavigation to use them

--- src/components/user/UserNavigation.tsx
+++ src/components/user/UserNavigation.tsx
@@ -1,4 +1,7 @@
+import { PostsIcon } from '@/assets/icons/PostsIcon';
 import Link from 'next/link';
+import { ReelsIcon } from '../../assets/icons/ReelsIcon';
+import { TaggedIcon } from '../../assets/icons/TaggedIcon';
 
 export const UserNavigation = () => {
   return (
@@ -8,70 +11,7 @@ export const UserNavigation = () => {
           className='flex items-center justify-center gap-2 py-3 border-t md:py-5 border-foreground'
           href={'/'}
         >
-          <svg
-            aria-label=''
-            className='w-6 h-6 md:w-3 md:h-3'
-            fill='currentColor'
-            role='img'
-            viewBox='0 0 24 24'
-          >
-            <title></title>
-            <rect
-              fill='none'
-              height='18'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              width='18'
-              x='3'
-              y='3'
-            ></rect>
-            <line
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              x1='9.015'
-              x2='9.015'
-              y1='3'
-              y2='21'
-            ></line>
-            <line
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              x1='14.985'
-              x2='14.985'
-              y1='3'
-              y2='21'
-            ></line>
-            <line
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              x1='21'
-              x2='3'
-              y1='9.015'
-              y2='9.015'
-            ></line>
-            <line
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              x1='21'
-              x2='3'
-              y1='14.985'
-              y2='14.985'
-            ></line>
-          </svg>
+          <PostsIcon />
           <span className='hidden text-xs font-semibold uppercase md:block'>
             Posts
           </span>
@@ -81,59 +21,7 @@ export const UserNavigation = () => {
           className='flex items-center justify-center gap-2 py-3 border-t md:py-5 border-foreground'
           href={'/reels'}
         >
-          <svg
-            aria-label=''
-            className='w-6 h-6 md:w-3 md:h-3'
-            fill='currentColor'
-            role='img'
-            viewBox='0 0 24 24'
-          >
-            <title></title>
-            <line
-              fill='none'
-              stroke='currentColor'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              x1='2.049'
-              x2='21.95'
-              y1='7.002'
-              y2='7.002'
-            ></line>
-            <line
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              x1='13.504'
-              x2='16.362'
-              y1='2.001'
-              y2='7.002'
-            ></line>
-            <line
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              x1='7.207'
-              x2='10.002'
-              y1='2.11'
-              y2='7.002'
-            ></line>
-            <path
-              d='M2 12.001v3.449c0 2.849.698 4.006 1.606 4.945.94.908 2.098 1.607 4.946 1.607h6.896c2.848 0 4.006-.699 4.946-1.607.908-.939 1.606-2.096 1.606-4.945V8.552c0-2.848-.698-4.006-1.606-4.945C19.454 2.699 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.546 2 5.704 2 8.552Z'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-            ></path>
-            <path
-              d='M9.763 17.664a.908.908 0 0 1-.454-.787V11.63a.909.909 0 0 1 1.364-.788l4.545 2.624a.909.909 0 0 1 0 1.575l-4.545 2.624a.91.91 0 0 1-.91 0Z'
-              fillRule='evenodd'
-            ></path>
-          </svg>
+          <ReelsIcon />
           <span className='hidden text-xs font-semibold uppercase md:block'>
             Reels
           </span>
@@ -143,41 +31,7 @@ export const UserNavigation = () => {
           className='flex items-center justify-center gap-2 py-3 border-t md:py-5 border-foreground'
           href={'/tagged'}
         >
-          <svg
-            aria-label=''
-            className='w-6 h-6 md:w-3 md:h-3'
-            fill='currentColor'
-            role='img'
-            viewBox='0 0 24 24'
-          >
-            <title></title>
-            <path
-              d='M10.201 3.797 12 1.997l1.799 1.8a1.59 1.59 0 0 0 1.124.465h5.259A1.818 1.818 0 0 1 22 6.08v14.104a1.818 1.818 0 0 1-1.818 1.818H3.818A1.818 1.818 0 0 1 2 20.184V6.08a1.818 1.818 0 0 1 1.818-1.818h5.26a1.59 1.59 0 0 0 1.123-.465Z'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-            ></path>
-            <path
-              d='M18.598 22.002V21.4a3.949 3.949 0 0 0-3.948-3.949H9.495A3.949 3.949 0 0 0 5.546 21.4v.603'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-            ></path>
-            <circle
-              cx='12.072'
-              cy='11.075'
-              fill='none'
-              r='3.556'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-            ></circle>
-          </svg>
+          <TaggedIcon />
           <span className='hidden text-xs font-semibold uppercase md:block'>
             Tagged
           </span>
