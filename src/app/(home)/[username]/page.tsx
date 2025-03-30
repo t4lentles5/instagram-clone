7ccf20c6be8e9090eb569783e748e55caa5f9150
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-30 11:28:13 -0500

    Add CameraIcon component and replace inline SVG in ProfileUserPage

--- src/app/(home)/[username]/page.tsx
+++ src/app/(home)/[username]/page.tsx
@@ -1,5 +1,6 @@
 import { getAuthenticatedUser } from '@/actions/auth/get-authenticate-user';
 import { getUserByUsername } from '@/actions/user/get-user-by-username';
+import { CameraIcon } from '@/assets/icons/CameraIcon';
 import { Footer } from '@/components/ui/Footer';
 import { HeaderPageMobile } from '@/components/ui/HeaderPageMobile';
 import { UserInfo } from '@/components/user/UserInfo';
@@ -65,44 +66,7 @@ export default async function ProfileUserPage({ params }: Props) {
         </div>
 
         <div className='flex flex-col items-center justify-center w-full h-full gap-5'>
-          <svg
-            aria-label='Camera'
-            fill='currentColor'
-            height='62'
-            role='img'
-            viewBox='0 0 96 96'
-            width='62'
-            className='text-text'
-          >
-            <title>Camera</title>
-            <circle
-              cx='48'
-              cy='48'
-              fill='none'
-              r='47'
-              stroke='currentColor'
-              strokeMiterlimit='10'
-              strokeWidth='2'
-            ></circle>
-            <ellipse
-              cx='48.002'
-              cy='49.524'
-              fill='none'
-              rx='10.444'
-              ry='10.476'
-              stroke='currentColor'
-              strokeLinejoin='round'
-              strokeWidth='2.095'
-            ></ellipse>
-            <path
-              d='M63.994 69A8.02 8.02 0 0 0 72 60.968V39.456a8.023 8.023 0 0 0-8.01-8.035h-1.749a4.953 4.953 0 0 1-4.591-3.242C56.61 25.696 54.859 25 52.469 25h-8.983c-2.39 0-4.141.695-5.181 3.178a4.954 4.954 0 0 1-4.592 3.242H32.01a8.024 8.024 0 0 0-8.012 8.035v21.512A8.02 8.02 0 0 0 32.007 69Z'
-              fill='none'
-              stroke='currentColor'
-              strokeLinejoin='round'
-              strokeWidth='2'
-            ></path>
-          </svg>
-
+          <CameraIcon />
           <p className='mt-4 text-3xl font-bold text-center text-text'>
             {isOwnProfile ? 'Share Photos' : 'No posts yet!'}
           </p>
