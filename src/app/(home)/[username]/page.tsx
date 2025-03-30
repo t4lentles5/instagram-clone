42c5ee62f90eeefb847534589f589f79d667ac8c
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-03-30 11:52:35 -0500

    Refactor user profile page and add new icon components for improved functionality and user experience

--- src/app/(home)/[username]/page.tsx
+++ src/app/(home)/[username]/page.tsx
@@ -39,7 +39,7 @@ export default async function ProfileUserPage({ params }: Props) {
     notFound();
   }
 
-  const isOwnProfile = userByUsername === authenticatedUser;
+  const isOwnProfile = userByUsername.username === authenticatedUser.username;
 
   return (
     <>
--- src/components/user/UserInfo.tsx
+++ src/components/user/UserInfo.tsx
@@ -2,9 +2,9 @@
 
 import { User } from '@/interfaces/user.interface';
 import { ProfilePhoto } from './ProfilePhoto';
-import { DotsThree } from 'phosphor-react';
 import Link from 'next/link';
-import { OptionsIcons } from '@/assets/icons/OptionsIcons';
+import { OptionsIcon } from '@/assets/icons/OptionsIcon';
+import { SimilarAccountsIcon } from '@/assets/icons/SimilarAccountsIcon';
 
 interface Props {
   userByUsername: User;
@@ -34,7 +34,7 @@ export const UserInfo = ({ userByUsername, authenticatedUser }: Props) => {
                   </Link>
 
                   <button>
-                    <OptionsIcons />
+                    <OptionsIcon isOwnProfile={isOwnProfile} />
                   </button>
                 </div>
               ) : (
@@ -47,62 +47,11 @@ export const UserInfo = ({ userByUsername, authenticatedUser }: Props) => {
                     </button>
 
                     <button className='px-2 font-semibold rounded-lg text-foreground bg-buttonSecondary hover:bg-buttonSecondaryHover'>
-                      <svg
-                        aria-label='Similar accounts'
-                        className='x1lliihq x1n2onr6 x5n08af'
-                        fill='currentColor'
-                        height='16'
-                        role='img'
-                        viewBox='0 0 24 24'
-                        width='16'
-                      >
-                        <title>Similar accounts</title>
-                        <path
-                          d='M19.006 8.252a3.5 3.5 0 1 1-3.499-3.5 3.5 3.5 0 0 1 3.5 3.5Z'
-                          fill='none'
-                          stroke='currentColor'
-                          strokeMiterlimit='10'
-                          strokeWidth='2'
-                        ></path>
-                        <path
-                          d='M22 19.5v-.447a4.05 4.05 0 0 0-4.05-4.049h-4.906a4.05 4.05 0 0 0-4.049 4.049v.447'
-                          fill='none'
-                          stroke='currentColor'
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          strokeWidth='2'
-                        ></path>
-                        <line
-                          fill='none'
-                          stroke='currentColor'
-                          strokeLinecap='round'
-                          strokeMiterlimit='10'
-                          strokeWidth='2'
-                          x1='5.001'
-                          x2='5.001'
-                          y1='7.998'
-                          y2='14.003'
-                        ></line>
-                        <line
-                          fill='none'
-                          stroke='currentColor'
-                          strokeLinecap='round'
-                          strokeMiterlimit='10'
-                          strokeWidth='2'
-                          x1='8.004'
-                          x2='2.003'
-                          y1='11'
-                          y2='11'
-                        ></line>
-                      </svg>
+                      <SimilarAccountsIcon />
                     </button>
                   </div>
 
-                  <DotsThree
-                    size={32}
-                    weight='bold'
-                    className='m-2 text-text'
-                  />
+                  <OptionsIcon isOwnProfile={isOwnProfile} />
                 </>
               )}
             </div>
@@ -112,54 +61,7 @@ export const UserInfo = ({ userByUsername, authenticatedUser }: Props) => {
               <button className='text-sm button-secondary'>Message</button>
 
               <button className='px-2 font-semibold rounded-lg text-foreground bg-buttonSecondary hover:bg-buttonSecondaryHover'>
-                <svg
-                  aria-label='Similar accounts'
-                  className='x1lliihq x1n2onr6 x5n08af'
-                  fill='currentColor'
-                  height='16'
-                  role='img'
-                  viewBox='0 0 24 24'
-                  width='16'
-                >
-                  <title>Similar accounts</title>
-                  <path
-                    d='M19.006 8.252a3.5 3.5 0 1 1-3.499-3.5 3.5 3.5 0 0 1 3.5 3.5Z'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeMiterlimit='10'
-                    strokeWidth='2'
-                  ></path>
-                  <path
-                    d='M22 19.5v-.447a4.05 4.05 0 0 0-4.05-4.049h-4.906a4.05 4.05 0 0 0-4.049 4.049v.447'
-                    fill='none'
-                    stroke='currentColor'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                  ></path>
-                  <line
-                    fill='none'
-                    stroke='currentColor'
-                    strokeLinecap='round'
-                    strokeMiterlimit='10'
-                    strokeWidth='2'
-                    x1='5.001'
-                    x2='5.001'
-                    y1='7.998'
-                    y2='14.003'
-                  ></line>
-                  <line
-                    fill='none'
-                    stroke='currentColor'
-                    strokeLinecap='round'
-                    strokeMiterlimit='10'
-                    strokeWidth='2'
-                    x1='8.004'
-                    x2='2.003'
-                    y1='11'
-                    y2='11'
-                  ></line>
-                </svg>
+                <SimilarAccountsIcon />
               </button>
             </div>
 
