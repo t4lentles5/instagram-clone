596e7d99ba9556561ce4b462015b5d2ff5e8b2c0
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-02 21:49:39 -0500

    Refactor UserNavigation component: improve JSX formatting and enhance styling consistency

--- src/components/user/UserNavigation.tsx
+++ src/components/user/UserNavigation.tsx
@@ -1,38 +1,38 @@
-import { PostsIcon } from '@/assets/icons/PostsIcon';
 import Link from 'next/link';
-import { ReelsIcon } from '../../assets/icons/ReelsIcon';
-import { TaggedIcon } from '../../assets/icons/TaggedIcon';
+import { PostsIcon } from '@/assets/icons/PostsIcon';
+import { ReelsIcon } from '@/assets/icons/ReelsIcon';
+import { TaggedIcon } from '@/assets/icons/TaggedIcon';
 
 export const UserNavigation = () => {
   return (
     <>
-      <div className='grid items-center w-full grid-cols-3 md:flex md:items-center md:justify-center md:gap-16'>
+      <div className="grid w-full grid-cols-3 items-center md:flex md:items-center md:justify-center md:gap-16">
         <Link
-          className='flex items-center justify-center gap-2 py-3 border-t md:py-5 border-foreground'
+          className="border-foreground flex items-center justify-center gap-2 border-t py-3 md:py-5"
           href={'/'}
         >
           <PostsIcon />
-          <span className='hidden text-xs font-semibold uppercase md:block'>
+          <span className="hidden text-xs font-semibold uppercase md:block">
             Posts
           </span>
         </Link>
 
         <Link
-          className='flex items-center justify-center gap-2 py-3 border-t md:py-5 border-foreground'
+          className="border-foreground flex items-center justify-center gap-2 border-t py-3 md:py-5"
           href={'/reels'}
         >
           <ReelsIcon />
-          <span className='hidden text-xs font-semibold uppercase md:block'>
+          <span className="hidden text-xs font-semibold uppercase md:block">
             Reels
           </span>
         </Link>
 
         <Link
-          className='flex items-center justify-center gap-2 py-3 border-t md:py-5 border-foreground'
+          className="border-foreground flex items-center justify-center gap-2 border-t py-3 md:py-5"
           href={'/tagged'}
         >
           <TaggedIcon />
-          <span className='hidden text-xs font-semibold uppercase md:block'>
+          <span className="hidden text-xs font-semibold uppercase md:block">
             Tagged
           </span>
         </Link>
