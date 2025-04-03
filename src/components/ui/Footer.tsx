03eacf4f7a2c865d858b42715ba1ee03d28df252
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-03 15:03:35 -0500

    Refactor Footer component: improve JSX formatting and enhance styling consistency

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export const Footer = () => {
   return (
     <>
-      <footer className='p-5 text-xs text-center text-foregroundSecondary'>
+      <footer className="text-foreground-secondary p-5 text-center text-xs">
         Cloned with 💙 by{' '}
         <Link
-          href='https://github.com/Obrn544'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-buttonPrimary'
+          href="https://github.com/Obrn544"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-foreground-secondary hover:underline"
         >
           @obrn544
         </Link>
