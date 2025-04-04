24f562265a600a854890e28f4600e98ebf457f34
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-04 18:54:11 -0500

    Refactor OrComponent: simplify class names for improved readability and consistency

--- src/components/ui/OrComponent.tsx
+++ src/components/ui/OrComponent.tsx
@@ -1,8 +1,8 @@
 export const OrComponent = () => {
   return (
     <>
-      <div className='flex items-center justify-center w-full h-0 border border-separator'>
-        <p className='px-4 text-xs bg-background'>OR</p>
+      <div className="border-border flex h-0 w-full items-center justify-center border">
+        <p className="bg-background px-4 text-xs">OR</p>
       </div>
     </>
   );
