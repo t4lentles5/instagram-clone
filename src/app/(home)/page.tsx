9c2510a4510b13e330baaaa8fd99df2983f27e35
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-03 19:36:47 -0500

    Add Post component to HomePage: replace user display with Post component for improved structure

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ import { SwitchUser } from '@/components/home/SwitchUser';
 import { Footer } from '@/components/ui/Footer';
 import { SuggestedUsers } from '@/components/home/suggested-users/SuggestedUsers';
 import { UserStories } from '@/components/home/user-stories/UserStories';
+import { Post } from '@/components/ui/Post';
 
 export default async function HomePage() {
   const user = await getAuthenticatedUser();
@@ -14,53 +15,7 @@ export default async function HomePage() {
       <div className="flex w-[630px] flex-col items-center pt-4">
         <UserStories users={users} />
         {users.map((user) => (
-          <div
-            key={user.id}
-            className="mx-auto w-full max-w-[470px] px-4 sm:px-0"
-          >
-            <div className="flex w-full items-center pb-[14px] pl-1">
-              <div className="mr-3">
-                <div className="h-8 w-8">
-                  <img
-                    className="h-8 w-8 rounded-full"
-                    src={user.profile_photo}
-                    alt="profile photo"
-                  />
-                </div>
-              </div>
-              <div className="flex w-full items-baseline gap-x-1">
-                <span className="text-sm leading-[18px]">{user.username}</span>
-
-                <span className="flex items-center justify-center">•</span>
-
-                <time
-                  dateTime="2025-03-31T15:30:23.000Z"
-                  className="text-foreground-secondary text-sm leading-[18px]"
-                  title="Mar 31, 2025"
-                >
-                  1d
-                </time>
-              </div>
-
-              <div className="flex w-8 justify-end">
-                <svg
-                  aria-label="More options"
-                  fill="currentColor"
-                  height="24"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  width="24"
-                >
-                  <title>More options</title>
-                  <circle cx="12" cy="12" r="1.5"></circle>
-                  <circle cx="6" cy="12" r="1.5"></circle>
-                  <circle cx="18" cy="12" r="1.5"></circle>
-                </svg>
-              </div>
-            </div>
-
-            <div className="min-[485px]:border-border aspect-468/585 w-full min-[485px]:rounded-[4px] min-[485px]:border"></div>
-          </div>
+          <Post user={user} key={user.id} />
         ))}
       </div>
 
