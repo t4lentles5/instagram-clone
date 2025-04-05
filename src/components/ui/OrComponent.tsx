1a98692a683076b93d3a9dcc369a89ea1040a452
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-05 17:47:34 -0500

    Refactor OrComponent: replace fragment with a div for improved structure

--- src/components/ui/OrComponent.tsx
+++ src/components/ui/OrComponent.tsx
@@ -1,9 +1,9 @@
 export const OrComponent = () => {
   return (
-    <>
+    <div className="px-10 py-2">
       <div className="border-border flex h-0 w-full items-center justify-center border">
         <p className="bg-background px-4 text-xs">OR</p>
       </div>
-    </>
+    </div>
   );
 };
