b4ebe90be4cb3ff88a499c09b7b13ef670ab555e
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-06 09:59:48 -0500

    Refactor SidebarMoreOptions: enhance styling consistency and readability

--- src/components/sidebar/SidebarMoreOptions.tsx
+++ src/components/sidebar/SidebarMoreOptions.tsx
@@ -60,10 +60,10 @@ export const SidebarMoreOptions = () => {
     <>
       <div
         onClick={handleOpen}
-        className='relative items-center justify-center hidden w-full cursor-pointer h-14 md:flex'
+        className="relative hidden h-14 w-full cursor-pointer items-center justify-center md:flex"
         ref={popoverRef}
       >
-        <button className='w-full nav-item'>
+        <button className="hover:bg-hover flex w-full cursor-pointer items-center justify-start gap-4 rounded-lg p-3 md:w-full">
           {open || switchAppearanceOpen ? (
             <MoreOptionsFillIcon />
           ) : (
@@ -72,9 +72,9 @@ export const SidebarMoreOptions = () => {
 
           <span
             className={`${
-              isSidebarCollapsed ? 'hidden ' : 'xl:block '
+              isSidebarCollapsed ? 'hidden' : 'xl:block'
             } hidden leading-5 ${
-              open || (switchAppearanceOpen && 'font-bold')
+              (open || switchAppearanceOpen) && 'font-bold'
             }`}
           >
             More
@@ -84,49 +84,49 @@ export const SidebarMoreOptions = () => {
         <div
           className={`${
             open
-              ? 'opacity-100 translate-y-[5px]'
-              : 'opacity-0 -translate-x-2 pointer-events-none'
-          } absolute w-[266px] p-2 shadow-2xl rounded-2xl gap-2 xl:mb-1 left-14 xl:left-0 bg-popover bottom-full transform ease-in-out`}
+              ? 'translate-y-[5px] opacity-100'
+              : 'pointer-events-none -translate-x-2 opacity-0'
+          } bg-popover absolute bottom-full left-14 w-[266px] transform gap-2 rounded-2xl p-2 shadow-2xl ease-in-out xl:left-0 xl:mb-1`}
         >
-          <div className='pb-2 border-b border-separator'>
+          <div className="border-border border-b pb-2">
             <Link
-              className={`flex items-center justify-start w-full gap-3 p-4 rounded-lg hover:bg-popoverHover`}
+              className={`hover:bg-popoverHover flex w-full items-center justify-start gap-3 rounded-lg p-4`}
               href={'/settings'}
             >
               <SettingsIcon />
-              <span className='text-sm leading-[18px]'>Setting</span>
+              <span className="text-sm leading-[18px]">Setting</span>
             </Link>
 
             <Link
-              className={`flex items-center justify-start w-full gap-3 p-4 rounded-lg hover:bg-popoverHover`}
+              className={`hover:bg-popoverHover flex w-full items-center justify-start gap-3 rounded-lg p-4`}
               href={'/saved'}
             >
               <SavedIcon />
-              <span className='text-sm leading-[18px]'>Saved</span>
+              <span className="text-sm leading-[18px]">Saved</span>
             </Link>
 
             <button
-              className={`flex items-center justify-start w-full gap-3 p-4 hover:rounded-lg hover:bg-popoverHover`}
+              className={`hover:bg-popoverHover flex w-full items-center justify-start gap-3 p-4 hover:rounded-lg`}
               onClick={handleSwitchAppearanceOpen}
             >
               {isDarkMode ? <MoonIcon /> : <SunIcon />}
-              <span className='text-sm leading-[18px]'>Switch appearance</span>
+              <span className="text-sm leading-[18px]">Switch appearance</span>
             </button>
           </div>
 
-          <div className='py-2 border-b border-separator'>
+          <div className="border-border border-b py-2">
             <button
-              className={`w-full text-sm leading-[18px] p-4 text-left cursor-pointer hover:rounded-lg hover:bg-popoverHover`}
-              type='button'
+              className={`hover:bg-popoverHover w-full cursor-pointer p-4 text-left text-sm leading-[18px] hover:rounded-lg`}
+              type="button"
               onClick={() => {}}
             >
               Switch accounts
             </button>
           </div>
-          <div className='mt-2'>
+          <div className="mt-2">
             <button
-              className={`w-full p-4 text-left text-sm leading-[18px] cursor-pointer hover:rounded-lg hover:bg-popoverHover`}
-              type='button'
+              className={`hover:bg-popoverHover w-full cursor-pointer p-4 text-left text-sm leading-[18px] hover:rounded-lg`}
+              type="button"
               onClick={handleLogout}
             >
               Log out
@@ -137,45 +137,45 @@ export const SidebarMoreOptions = () => {
         <div
           className={`${
             switchAppearanceOpen
-              ? 'opacity-100 translate-y-[4px]'
-              : 'opacity-0 -translate-x-2 pointer-events-none'
-          } absolute w-[266px]  shadow-2xl rounded-2xl  xl:mb-1 left-14 xl:left-0 bg-popover bottom-full transform ease-in-out`}
+              ? 'translate-y-[4px] opacity-100'
+              : 'pointer-events-none -translate-x-2 opacity-0'
+          } bg-popover absolute bottom-full left-14 w-[266px] transform rounded-2xl shadow-2xl ease-in-out xl:left-0 xl:mb-1`}
         >
-          <div className='flex items-center w-full p-4 border-b border-[#3d3d3d] hover:rounded-lg'>
+          <div className="border-border flex w-full items-center border-b p-4 hover:rounded-lg">
             <button
-              className='w-6 text-[#6a6a6a]'
+              className="w-6 text-[#6a6a6a]"
               onClick={handleSwitchAppearanceOpen}
             >
               <BackIcon />
             </button>
-            <span className='text-left text-base font-semibold w-[180px] leading-5'>
+            <span className="w-[180px] text-left text-base leading-5 font-semibold">
               Switch appearance
             </span>
-            <div className='w-[30px] flex justify-end'>
+            <div className="flex w-[30px] justify-end">
               {isDarkMode ? <MoonSmallIcon /> : <SunSmallIcon />}
             </div>
           </div>
 
-          <div className='flex justify-center p-2 w-full items-'>
+          <div className="items- flex w-full justify-center p-2">
             <button
-              className='flex items-center justify-between w-full p-4 text-left rounded-lg cursor-pointer hover:bg-popoverHover'
+              className="hover:bg-popoverHover flex w-full cursor-pointer items-center justify-between rounded-lg p-4 text-left"
               onClick={(e) => {
                 e.stopPropagation();
                 toggleTheme();
               }}
             >
-              <span className='text-left text-sm leading-[18px]'>
+              <span className="text-left text-sm leading-[18px]">
                 Dark mode
               </span>
 
-              <label className='inline-flex items-center ml-2'>
+              <label className="ml-2 inline-flex items-center">
                 <input
-                  type='checkbox'
+                  type="checkbox"
                   checked={isDarkMode}
                   onChange={toggleTheme}
-                  className='sr-only peer'
+                  className="peer sr-only"
                 />
-                <div className='relative w-[26px] h-4 bg-foreground rounded-full peer-checked:after:translate-x-[10px] after:absolute after:top-[2px] after:start-[2px] after:bg-background after:rounded-full after:h-3 after:w-3 after:transition-all'></div>
+                <div className="bg-foreground after:bg-background relative h-4 w-[26px] rounded-full after:absolute after:start-[2px] after:top-[2px] after:h-3 after:w-3 after:rounded-full after:transition-all peer-checked:after:translate-x-[10px]"></div>
               </label>
             </button>
           </div>
