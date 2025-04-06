209d9677b4b02752d911f83cba837f6ee6ba45b1
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-06 16:39:00 -0500

    Refactor SignUpForm: extract input fields into SignUpFormInput component for improved readability and maintainability

--- src/components/auth/signup/SignUpForm.tsx
+++ src/components/auth/signup/SignUpForm.tsx
@@ -1,17 +1,17 @@
 'use client';
 
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { CheckCircle, XCircle } from 'phosphor-react';
 import {
   emailVerify,
   registerUser,
   usernameVerify,
 } from '@/actions/auth/register';
 import { login } from '@/actions/auth/login';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { SignUpFormInput } from '@/components/auth/signup/SignUpFormInput';
 
-interface FormInputs {
+export interface SignUpFormInputs {
   email: string;
   password: string;
   fullname: string;
@@ -28,9 +28,9 @@ export const SignUpForm = () => {
     watch,
     formState: { errors, isValid },
     setError,
-  } = useForm<FormInputs>();
+  } = useForm<SignUpFormInputs>();
 
-  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+  const onSubmit: SubmitHandler<SignUpFormInputs> = async (data) => {
     const { email, password, fullname, username } = data;
 
     const emailResp = await emailVerify(email);
@@ -64,214 +64,79 @@ export const SignUpForm = () => {
     router.push('/');
   };
 
-  const emailValue = watch('email');
-  const passwordValue = watch('password');
-  const fullnameValue = watch('fullname');
-  const usernameValue = watch('username');
-
   return (
     <>
       <form className="w-ful mt-3 mb-5" onSubmit={handleSubmit(onSubmit)}>
-        <div className="border-input-border bg-input-bg mx-10 mb-[6px] flex h-[36px] flex-col gap-3 rounded-[3px] border">
-          <div
-            className={`bg-input-bg flex h-10 w-full gap-3 text-xs ${
-              errors.email ? 'border-red-500' : 'border-border'
-            }`}
-          >
-            <label className="flex h-full w-4/5 flex-col items-center px-2">
-              {emailValue && (
-                <span className="bg-input-bg text-secondary flex w-full items-center text-[9px]">
-                  Email
-                </span>
-              )}
+        <SignUpFormInput
+          value={'email'}
+          register={register}
+          errors={errors.email}
+          placeholder={'Email'}
+          type={'email'}
+          watch={watch}
+          pattern={{
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: 'Please enter a valid email address.',
+          }}
+        />
 
-              <input
-                type="email"
-                className={`text-primary bg-input-bg h-full w-full border-none focus:outline-hidden ${emailValue && 'text-xs leading-3'}`}
-                placeholder={'Email'}
-                {...register('email', {
-                  required: { value: true, message: 'Email is required.' },
-                  pattern: {
-                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                    message: 'Please enter a valid email address.',
-                  },
-                })}
-              />
-            </label>
+        <SignUpFormInput
+          value={'password'}
+          register={register}
+          errors={errors.password}
+          placeholder={'Password'}
+          type={!showPassword ? 'password' : 'text'}
+          watch={watch}
+          minLength={{
+            value: 6,
+            message: 'Password must have at least 6 characters.',
+          }}
+          showPassword={showPassword}
+          setShowPassword={setShowPassword}
+        />
 
-            {emailValue && (
-              <div className="flex w-1/5 items-center justify-center">
-                {errors.email ? (
-                  <XCircle size={24} className="text-red-500" />
-                ) : (
-                  <CheckCircle size={24} className="" />
-                )}
-              </div>
-            )}
-          </div>
-          {errors.email && (
-            <span className="text-xs text-red-500">{errors.email.message}</span>
-          )}
-        </div>
+        <SignUpFormInput
+          value={'fullname'}
+          register={register}
+          errors={errors.fullname}
+          placeholder={'Fullname'}
+          type={'text'}
+          watch={watch}
+          pattern={{
+            value: /^[a-zA-Z\s]+$/,
+            message: 'Fullname can only contain letters and spaces.',
+          }}
+          minLength={{
+            value: 3,
+            message: 'Fullname must be at least 3 characters long.',
+          }}
+          maxLength={{
+            value: 20,
+            message: 'Fullname cannot exceed 20 characters.',
+          }}
+        />
 
-        <div className="border-input-border bg-input-bg mx-10 mb-[6px] flex h-[36px] flex-col gap-3 rounded-[3px] border">
-          <div
-            className={`bg-input-bg flex h-10 w-full gap-3 text-xs ${
-              errors.password ? 'border-red-500' : 'border-border'
-            }`}
-          >
-            <label className="flex h-full w-4/5 flex-col items-center px-2">
-              {passwordValue && (
-                <span className="bg-input-bg text-secondary flex w-full items-center text-[9px]">
-                  Password
-                </span>
-              )}
-
-              <input
-                type={!showPassword ? 'password' : 'text'}
-                className={`text-primary bg-input-bg h-full w-full border-none focus:outline-hidden ${passwordValue && 'text-xs leading-3'}`}
-                placeholder={'Password'}
-                {...register('password', {
-                  required: { value: true, message: 'Password is required.' },
-                  minLength: {
-                    value: 6,
-                    message: 'Password must have at least 6 characters.',
-                  },
-                })}
-              />
-            </label>
-
-            {passwordValue && (
-              <div className="flex w-1/4 items-center justify-center">
-                {errors.password ? (
-                  <XCircle size={24} className="text-red-500" />
-                ) : (
-                  <CheckCircle size={24} className="" />
-                )}
-
-                <button
-                  className="pl-2 font-semibold"
-                  type="button"
-                  onClick={() => {
-                    setShowPassword(!showPassword);
-                  }}
-                >
-                  {showPassword ? 'Hide' : 'Show'}
-                </button>
-              </div>
-            )}
-          </div>
-          {errors.password && (
-            <span className="text-xs text-red-500">
-              {errors.password.message}
-            </span>
-          )}
-        </div>
-
-        <div className="border-input-border bg-input-bg mx-10 mb-[6px] flex h-[36px] flex-col gap-3 rounded-[3px] border">
-          <div
-            className={`bg-input-bg flex h-10 w-full gap-3 text-xs ${
-              errors.fullname ? 'border-red-500' : 'border-border'
-            }`}
-          >
-            <label className="flex h-full w-4/5 flex-col items-center px-2">
-              {fullnameValue && (
-                <span className="bg-input-bg text-secondary flex w-full items-center text-[9px]">
-                  Fullname
-                </span>
-              )}
-
-              <input
-                type="text"
-                className={`text-primary bg-input-bg h-full w-full border-none focus:outline-hidden ${fullnameValue && 'text-xs leading-3'}`}
-                placeholder={'Fullname'}
-                {...register('fullname', {
-                  required: { value: true, message: 'Fullname is required.' },
-                  minLength: {
-                    value: 3,
-                    message: 'Fullname must be at least 3 characters long.',
-                  },
-                  maxLength: {
-                    value: 20,
-                    message: 'Fullname cannot exceed 20 characters.',
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z\s]+$/,
-                    message: 'Fullname can only contain letters and spaces.',
-                  },
-                })}
-              />
-            </label>
-
-            {fullnameValue && (
-              <div className="flex w-1/5 items-center justify-center">
-                {errors.fullname ? (
-                  <XCircle size={24} className="text-red-500" />
-                ) : (
-                  <CheckCircle size={24} className="" />
-                )}
-              </div>
-            )}
-          </div>
-          {errors.fullname && (
-            <span className="text-xs text-red-500">
-              {errors.fullname.message}
-            </span>
-          )}
-        </div>
-
-        <div className="border-input-border bg-input-bg mx-10 mb-[6px] flex h-[36px] flex-col gap-3 rounded-[3px] border">
-          <div
-            className={`bg-input-bg flex h-10 w-full gap-3 text-xs ${
-              errors.username ? 'border-red-500' : 'border-border'
-            }`}
-          >
-            <label className="flex h-full w-4/5 flex-col items-center px-2">
-              {usernameValue && (
-                <span className="bg-input-bg text-secondary flex w-full items-center text-[9px]">
-                  Username
-                </span>
-              )}
-
-              <input
-                type="text"
-                className={`text-primary bg-input-bg h-full w-full border-none focus:outline-hidden ${usernameValue && 'text-xs leading-3'}`}
-                placeholder={'Username'}
-                {...register('username', {
-                  required: { value: true, message: 'Username is required.' },
-                  minLength: {
-                    value: 3,
-                    message: 'Username must be at least 3 characters long.',
-                  },
-                  maxLength: {
-                    value: 20,
-                    message: 'Username cannot exceed 20 characters.',
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z0-9]+([._][a-zA-Z0-9]+)*$/,
-                    message:
-                      'Username can only contain letters, numbers, dots and underscores. It cannot start or end with a special character.',
-                  },
-                })}
-              />
-            </label>
-
-            {usernameValue && (
-              <div className="flex w-1/5 items-center justify-center">
-                {errors.username ? (
-                  <XCircle size={24} className="text-red-500" />
-                ) : (
-                  <CheckCircle size={24} className="" />
-                )}
-              </div>
-            )}
-          </div>
-          {errors.username && (
-            <span className="text-xs text-red-500">
-              {errors.username.message}
-            </span>
-          )}
-        </div>
+        <SignUpFormInput
+          value={'username'}
+          register={register}
+          errors={errors.username}
+          placeholder={'Username'}
+          type={'text'}
+          watch={watch}
+          pattern={{
+            value: /^[a-zA-Z0-9._]+$/,
+            message:
+              'Username can only contain letters, numbers, dots and underscores. It cannot start or end with a special character.',
+          }}
+          minLength={{
+            value: 3,
+            message: 'Username must be at least 3 characters long.',
+          }}
+          maxLength={{
+            value: 20,
+            message: 'Username cannot exceed 20 characters.',
+          }}
+        />
 
         <div className="w-full px-10 py-2">
           <button
---
+++ src/components/auth/signup/SignUpFormInput.tsx
@@ -0,0 +1,103 @@
+import { Dispatch, SetStateAction } from 'react';
+import { FieldError, UseFormRegister, UseFormWatch } from 'react-hook-form';
+import { CheckCircle, XCircle } from 'phosphor-react';
+import { SignUpFormInputs } from '@/components/auth/signup/SignUpForm';
+
+interface Props {
+  value: 'email' | 'password' | 'fullname' | 'username';
+  register: UseFormRegister<SignUpFormInputs>;
+  errors: FieldError | undefined;
+  placeholder: string;
+  type: string;
+  watch: UseFormWatch<SignUpFormInputs>;
+  showPassword?: boolean;
+  setShowPassword?: Dispatch<SetStateAction<boolean>>;
+  pattern?: {
+    value: RegExp;
+    message: string;
+  };
+  minLength?: {
+    value: number;
+    message: string;
+  };
+  maxLength?: { value: number; message: string };
+}
+
+export const SignUpFormInput = ({
+  value,
+  register,
+  errors,
+  placeholder,
+  type,
+  watch,
+  showPassword,
+  setShowPassword,
+  pattern,
+  minLength,
+  maxLength,
+}: Props) => {
+  const inputValue = watch(value);
+
+  return (
+    <>
+      <div className="mx-10 mb-[6px]">
+        <div
+          className={`bg-input-bg flex h-[36px] flex-col gap-3 rounded-[3px] border ${
+            errors ? 'border-red-500' : 'border-input-border'
+          }`}
+        >
+          <div className="bg-input-bg flex h-10 w-full gap-3 text-xs">
+            <label className="flex h-full w-4/5 flex-col items-center px-2">
+              {inputValue && (
+                <span className="bg-input-bg text-secondary flex w-full grow items-center text-[9px]">
+                  {placeholder}
+                </span>
+              )}
+
+              <input
+                type={type}
+                className={`text-primary bg-input-bg h-4 w-full grow border-none focus:outline-hidden ${inputValue && 'text-xs leading-3'}`}
+                placeholder={placeholder}
+                {...register(value, {
+                  required: {
+                    value: true,
+                    message: `${placeholder} is required.`,
+                  },
+                  pattern: pattern,
+                  minLength: minLength,
+                  maxLength: maxLength,
+                })}
+              />
+            </label>
+
+            {value === 'password' && setShowPassword && inputValue && (
+              <div className="flex w-1/4 items-center justify-center">
+                <button
+                  className="cursor-pointer pl-2 font-semibold"
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? 'Hide' : 'Show'}
+                </button>
+              </div>
+            )}
+
+            {inputValue && (
+              <div className="flex w-1/5 items-center justify-center">
+                {errors ? (
+                  <XCircle size={24} className="text-red-500" />
+                ) : (
+                  <CheckCircle size={24} className="" />
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+
+        {errors && (
+          <span className="text-xs text-red-500">{errors.message}</span>
+        )}
+      </div>
+    </>
+  );
+};
