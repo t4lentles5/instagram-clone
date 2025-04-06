e787907867a5b02b5961dbd79d5947a9ca5d6280
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-06 09:57:39 -0500

    Refactor ProfileUserPage: remove unused imports and simplify component structure

--- src/app/(home)/[username]/page.tsx
+++ src/app/(home)/[username]/page.tsx
@@ -1,30 +1,3 @@
-import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import { getAuthenticatedUser } from '@/actions/auth/get-authenticate-user';
-import { getUserByUsername } from '@/actions/user/get-user-by-username';
-import { CameraIcon } from '@/assets/icons/CameraIcon';
-import { Footer } from '@/components/ui/Footer';
-import { UserInfo } from '@/components/user/UserInfo';
-import { UserNavigation } from '@/components/user/UserNavigation';
-import { UserStats } from '@/components/user/UserStats';
-import { HeaderPageMobile } from '@/components/ui/HeaderPageMobile';
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { username } = await params;
-  const authenticatedUser = await getAuthenticatedUser();
-  const userByUsername = await getUserByUsername(username);
-
-  const user =
-    userByUsername.username === authenticatedUser.username
-      ? authenticatedUser
-      : userByUsername;
-
-  return {
-    title: `${user.fullname} (@${user.username}) • Instagram`,
-    description: `${user.fullname} (@${user.username}) • Instagram`,
-  };
-}
-
 interface Props {
   params: Promise<{ username: string }>;
 }
@@ -32,57 +5,34 @@ interface Props {
 export default async function ProfileUserPage({ params }: Props) {
   const { username } = await params;
 
-  const authenticatedUser = await getAuthenticatedUser();
-  const userByUsername = await getUserByUsername(username);
-
-  if (!userByUsername) {
-    notFound();
-  }
-
-  const isOwnProfile = userByUsername.username === authenticatedUser.username;
+  console.log(username);
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center lg:max-w-[935px] pt-16 md:pt-0 w-full md:mt-8 lg:mx-10'>
-        <HeaderPageMobile>
-          {isOwnProfile ? authenticatedUser.username : userByUsername.username}
-        </HeaderPageMobile>
+      <div className="grid w-full grid-cols-3 gap-1">
+        <div className="aspect-3/4 bg-purple-500">1</div>
+        <div className="aspect-3/4 bg-purple-500">1</div>
+        <div className="aspect-3/4 bg-purple-500">1</div>
+        <div className="aspect-3/4 bg-purple-500">1</div>
+        <div className="aspect-3/4 bg-purple-500">1</div>
+      </div>
 
-        <UserInfo
-          userByUsername={userByUsername}
-          authenticatedUser={authenticatedUser}
-        />
-
-        <UserStats />
-
-        <UserNavigation />
-
-        <div className='grid w-full grid-cols-3 gap-1'>
-          <div className='bg-purple-500 aspect-[3/4]'>1</div>
-          <div className='bg-purple-500 aspect-[3/4]'>1</div>
-          <div className='bg-purple-500 aspect-[3/4]'>1</div>
-          <div className='bg-purple-500 aspect-[3/4]'>1</div>
-          <div className='bg-purple-500 aspect-[3/4]'>1</div>
-        </div>
-
-        <div className='flex flex-col items-center justify-center w-full h-full gap-5'>
+      {/* <div className="flex h-full w-full flex-col items-center justify-center gap-5">
           <CameraIcon />
-          <p className='mt-4 text-3xl font-bold text-center text-text'>
+          <p className="mt-4 text-center text-3xl font-bold">
             {isOwnProfile ? 'Share Photos' : 'No posts yet!'}
           </p>
           {isOwnProfile && (
             <>
-              <p className='text-sm font-normal text-text'>
+              <p className="text-sm font-normal">
                 When you share photos, they will appear on your profile.
               </p>
-              <button className='text-sm font-normal text-primary'>
+              <button className="text-ig-blue text-sm font-normal">
                 Share your first photo
               </button>
             </>
           )}
-        </div>
-        <Footer />
-      </div>
+        </div> */}
     </>
   );
 }
