273dbecca31f91330e0aade3915f308186438eb2
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-07 15:42:19 -0500

    Refactor SidebarNav: extract profile section into SidebarNavItemProfile component for improved readability and maintainability

--- src/components/sidebar/SidebarNav.tsx
+++ src/components/sidebar/SidebarNav.tsx
@@ -1,30 +1,23 @@
-'use client';
-
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useSidebarStore } from '@/store/ui/sidebar-store';
-import { SearchButton } from '@/components/sidebar/SearchButton';
-import { NotificationsButton } from '@/components/sidebar/NotificationsButton';
-import { NewPostButton } from '@/components/sidebar/NewPostButton';
-import { User } from '@/interfaces/user.interface';
+import { ExploreFillIcon } from '@/assets/icons/sidebar/sidebar-nav/ExploreFillIcon';
+import { ExploreIcon } from '@/assets/icons/sidebar/sidebar-nav/ExploreIcon';
 import { HouseFillIcon } from '@/assets/icons/sidebar/sidebar-nav/HouseFillIcon';
 import { HouseIcon } from '@/assets/icons/sidebar/sidebar-nav/HouseIcon';
-import { ExploreIcon } from '@/assets/icons/sidebar/sidebar-nav/ExploreIcon';
-import { ExploreFillIcon } from '@/assets/icons/sidebar/sidebar-nav/ExploreFillIcon';
-import { ReelsIFillIcon } from '@/assets/icons/sidebar/sidebar-nav/ReelsIFillIcon';
-import { ReelsIcon } from '@/assets/icons/sidebar/sidebar-nav/ReelsIcon';
 import { MessengerFillIcon } from '@/assets/icons/sidebar/sidebar-nav/MessengerFillIcon';
 import { MessengerIcon } from '@/assets/icons/sidebar/sidebar-nav/MessengerIcon';
+import { NewPostButton } from '@/components/sidebar/NewPostButton';
+import { NotificationsButton } from '@/components/sidebar/NotificationsButton';
+import { ReelsIcon } from '@/assets/icons/sidebar/sidebar-nav/ReelsIcon';
+import { ReelsIFillIcon } from '@/assets/icons/sidebar/sidebar-nav/ReelsIFillIcon';
+import { SearchButton } from '@/components/sidebar/SearchButton';
 import { SidebarNavItem } from '@/components/sidebar/SidebarNavItem';
+import { SidebarNavItemProfile } from '@/components/sidebar/SidebarNavItemProfile';
+import { User } from '@/interfaces/user.interface';
 
 interface Props {
   user: User;
 }
 
 export const SidebarNav = ({ user }: Props) => {
-  const pathname = usePathname();
-  const { isSidebarCollapsed } = useSidebarStore();
-
   return (
     <nav className="border-border flex justify-evenly border-t md:flex-col md:border-0">
       <SidebarNavItem
@@ -61,34 +54,7 @@ export const SidebarNav = ({ user }: Props) => {
 
       <NewPostButton />
 
-      <div className="flex h-12 items-center justify-center md:h-14 md:w-full">
-        <Link
-          className="hover:bg-background-hover relative flex items-center justify-start gap-4 rounded-lg p-3 md:w-full"
-          href={`/${user.username}`}
-        >
-          <div className="relative">
-            {pathname === `/${user.username}` && (
-              <div className="border-foreground absolute top-0 left-0 h-7 w-7 translate-x-[-2px] translate-y-[-2px] rounded-full border-2"></div>
-            )}
-            <img
-              src={user.profile_photo}
-              alt="user profile photo"
-              className="h-6 w-6 rounded-full object-contain"
-            />
-          </div>
-          <span
-            className={`${
-              isSidebarCollapsed ? 'hidden' : 'xl:block'
-            } hidden leading-5 ${
-              pathname === `/${user.username}` &&
-              !isSidebarCollapsed &&
-              'font-bold'
-            }`}
-          >
-            Profile
-          </span>
-        </Link>
-      </div>
+      <SidebarNavItemProfile user={user} />
     </nav>
   );
 };
---
+++ src/components/sidebar/SidebarNavItemProfile.tsx
@@ -0,0 +1,46 @@
+import { User } from '@/interfaces/user.interface';
+import { useSidebarStore } from '@/store/ui/sidebar-store';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+interface Props {
+  user: User;
+}
+
+export const SidebarNavItemProfile = ({ user }: Props) => {
+  const pathname = usePathname();
+  const { isSidebarCollapsed } = useSidebarStore();
+
+  return (
+    <>
+      <div className="flex h-12 items-center justify-center md:h-14 md:w-full">
+        <Link
+          className="hover:bg-background-hover relative flex items-center justify-start gap-4 rounded-lg p-3 md:w-full"
+          href={`/${user.username}`}
+        >
+          <div className="relative">
+            {pathname === `/${user.username}` && (
+              <div className="border-foreground absolute top-0 left-0 h-7 w-7 translate-x-[-2px] translate-y-[-2px] rounded-full border-2"></div>
+            )}
+            <img
+              src={user.profile_photo}
+              alt="user profile photo"
+              className="h-6 w-6 rounded-full object-contain"
+            />
+          </div>
+          <span
+            className={`${
+              isSidebarCollapsed ? 'hidden' : 'xl:block'
+            } hidden leading-5 ${
+              pathname === `/${user.username}` &&
+              !isSidebarCollapsed &&
+              'font-bold'
+            }`}
+          >
+            Profile
+          </span>
+        </Link>
+      </div>
+    </>
+  );
+};
