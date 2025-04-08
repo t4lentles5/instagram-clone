80a46e0f79ae7e90ca9f39ff513d2db9a1c1e260
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-08 15:40:29 -0500

    Add seed data for users with profile photos and hashed passwords

--- src/seed/seed-users.ts
+++ src/seed/seed-users.ts
@@ -1,5 +1,3 @@
-import bcrypt from 'bcrypt';
-
 interface User {
   email: string;
   fullname: string;
@@ -13,70 +11,70 @@ export const users: User[] = [
     email: 'correo1@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_1',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_1.jpg',
   },
   {
     email: 'correo2@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_2',
-    password: bcrypt.hashSync('123456', 10),
-    profile_photo: '/profile_photos/photo_1.webp',
+    password: '123456',
+    profile_photo: '/profile_photos/photo_2.webp',
   },
   {
     email: 'correo3@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_3',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_3.webp',
   },
   {
     email: 'correo4@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_4',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_4.png',
   },
   {
     email: 'correo5@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_5',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_5.jpg',
   },
   {
     email: 'correo6@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_6',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_6.webp',
   },
   {
     email: 'correo7@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_7',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_7.jpg',
   },
   {
     email: 'correo8@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_8',
-    password: bcrypt.hashSync('123456', 10),
-    profile_photo: '',
+    password: '123456',
+    profile_photo: '/profile_photos/photo_8.webp',
   },
   {
     email: 'correo9@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_9',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_9.jpg',
   },
   {
     email: 'correo10@correo.com',
     fullname: 'Omar Obregon',
     username: 'omar_10',
-    password: bcrypt.hashSync('123456', 10),
+    password: '123456',
     profile_photo: '/profile_photos/photo_10.jpg',
   },
 ];
