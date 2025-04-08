e6f18c27ec979c224912a445f9c12dc06d7d2c41
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-08 15:39:55 -0500

    Move upload-profile-photo API route to users directory and add error handling

---
+++ src/app/api/users/upload-profile-photo/route.tsx
@@ -0,0 +1,30 @@
+import { changeProfilePhoto } from '@/actions/user/change-profile-photo';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get('image') as File;
+    const username = formData.get('username') as string;
+
+    if (!file || !username) {
+      return NextResponse.json(
+        { error: 'Missing image or username' },
+        { status: 400 },
+      );
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const base64Image = buffer.toString('base64');
+
+    const response = await changeProfilePhoto(base64Image, username);
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error('[UPLOAD_ERROR]', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 },
+    );
+  }
+}
