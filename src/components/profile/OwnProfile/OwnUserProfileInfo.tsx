72298be110aff04f402376ec62968e257222c17d
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-09 15:43:53 -0500

    Add OptionsIcon component and update OwnUserProfileInfo to use it; enhance button styles for better UX

--- src/components/profile/OwnProfile/OwnUserProfileInfo.tsx
+++ src/components/profile/OwnProfile/OwnUserProfileInfo.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { User } from '@/interfaces/user.interface';
 import { OwnProfilePhoto } from '@/components/profile/OwnProfile/OwnProfilePhoto';
 import { SimilarAccountsIcon } from '@/assets/icons/profile/SimilarAccountsIcon';
+import { OptionsIcon } from '@/assets/icons/profile/OptionsIcon';
 
 interface Props {
   user: User;
@@ -25,39 +26,13 @@ export const OwnUserProfileInfo = ({ user }: Props) => {
               <div className="flex items-center justify-center gap-2">
                 <Link
                   href={''}
-                  className="text-primary rounded-lg px-4 py-[6px] font-semibold"
+                  className="bg-button-secondary hover:bg-button-secondary-hover cursor-pointer rounded-lg px-4 py-[6px] text-sm font-semibold"
                 >
                   Edit Profile
                 </Link>
 
                 <button>
-                  <svg
-                    aria-label="Options"
-                    fill="currentColor"
-                    height="24"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    width="24"
-                  >
-                    <title>Options</title>
-                    <circle
-                      cx="12"
-                      cy="12"
-                      fill="none"
-                      r="8.635"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                    ></circle>
-                    <path
-                      d="M14.232 3.656a1.269 1.269 0 0 1-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 0 1-.796.66m-.001 16.688a1.269 1.269 0 0 1 .796.66l.505.996h1.862l.505-.996a1.269 1.269 0 0 1 .796-.66M3.656 9.768a1.269 1.269 0 0 1-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 0 1 .66.796m16.688-.001a1.269 1.269 0 0 1 .66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 0 1-.66-.796M7.678 4.522a1.269 1.269 0 0 1-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 0 1-.096 1.03m11.8 11.799a1.269 1.269 0 0 1 1.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 0 1 .096-1.03m-14.956.001a1.269 1.269 0 0 1 .096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 0 1 1.03.096m11.799-11.8a1.269 1.269 0 0 1-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 0 1-1.03-.096"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                    ></path>
-                  </svg>
+                  <OptionsIcon />
                 </button>
               </div>
             </div>
