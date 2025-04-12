2cee08ffc61f87eb616e4237a8c4476d6f15887f
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-12 11:26:26 -0500

    integrate post icons and update post rendering logic

--- src/app/(home)/[username]/page.tsx
+++ src/app/(home)/[username]/page.tsx
@@ -1,24 +1,53 @@
-import { getAuthenticatedUser } from '@/actions/auth/get-authenticated-user';
+import { getPostsByUsername } from '@/actions/user/get-posts-by-username';
 import { CameraIcon } from '@/features/profile/icons/CameraIcon';
+import { CarouselIcon } from '@/features/profile/icons/CarouselIcon';
+import { CommentsIcon } from '@/features/profile/icons/CommentsIcon';
+import { LikesIcon } from '@/features/profile/icons/LikesIcon';
 
 interface Props {
   params: Promise<{ username: string }>;
 }
 
 export default async function ProfileUserPage({ params }: Props) {
-  const user = await getAuthenticatedUser();
+  const { username } = await params;
+
+  const posts = await getPostsByUsername(username);
 
   return (
     <>
-      {user.posts.length ? (
+      {posts.length ? (
         <div className="grid w-full grid-cols-3 gap-1">
-          {user.posts.map((post) => (
-            <div key={post.id} className="">
+          {posts.map((post) => (
+            <div key={post.id} className="group relative">
               <img
                 src={post.PostImages[0].imageUrl}
                 alt=""
                 className="aspect-3/4 object-cover"
               />
+
+              {post.PostImages.length > 1 && (
+                <div className="absolute top-2 right-2">
+                  <CarouselIcon />
+                </div>
+              )}
+
+              <div className="bg-post-image-overlay invisible absolute top-0 left-0 h-full w-full opacity-0 group-hover:visible group-hover:opacity-100">
+                <div className="flex h-full items-center justify-center gap-8">
+                  <span className="flex items-center justify-center">
+                    <span className="mt-0.5 mr-[7px]">
+                      <LikesIcon />
+                    </span>
+                    <span className="text-base leading-5 font-bold">100</span>
+                  </span>
+
+                  <span className="flex items-center justify-center">
+                    <span className="mt-0.5 mr-[7px]">
+                      <CommentsIcon />
+                    </span>
+                    <span className="text-base leading-5 font-bold">10</span>
+                  </span>
+                </div>
+              </div>
             </div>
           ))}
         </div>
