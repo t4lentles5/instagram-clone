159e113580a05bdba41586ecbff95b9035124b51
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-16 18:54:44 -0500

    feat: add new icon components for Explore, Home, Messenger, and Reels; refactor SidebarNav to use new icons

--- src/features/home/sidebar/components/SidebarNav.tsx
+++ src/features/home/sidebar/components/SidebarNav.tsx
@@ -1,17 +1,13 @@
-import { ExploreFillIcon } from '@/features/home/sidebar/icons/sidebar-nav/ExploreFillIcon';
-import { ExploreIcon } from '@/features/home/sidebar/icons/sidebar-nav/ExploreIcon';
-import { HouseFillIcon } from '@/features/home/sidebar/icons/sidebar-nav/HouseFillIcon';
-import { HouseIcon } from '@/features/home/sidebar/icons/sidebar-nav/HouseIcon';
-import { MessengerFillIcon } from '@/features/home/sidebar/icons/sidebar-nav/MessengerFillIcon';
-import { MessengerIcon } from '@/features/home/sidebar/icons/sidebar-nav/MessengerIcon';
 import { NotificationsButton } from '@/features/home/sidebar/components/NotificationsButton';
-import { ReelsIcon } from '@/features/home/sidebar/icons/sidebar-nav/ReelsIcon';
-import { ReelsIFillIcon } from '@/features/home/sidebar/icons/sidebar-nav/ReelsIFillIcon';
 import { SearchButton } from '@/features/home/sidebar/components/SearchButton';
 import { SidebarNavItem } from '@/features/home/sidebar/components/SidebarNavItem';
 import { User } from '@/interfaces/user.interface';
 import { NewPostButton } from '@/features/home/sidebar/components/NewPostButton';
 import { SidebarNavItemProfile } from '@/features/home/sidebar/components/SidebarNavItemProfile';
+import { HomeIcon } from '@/components/icons/HomeIcon';
+import { ExploreIcon } from '@/components/icons/ExploreIcon';
+import { ReelsIcon } from '@/components/icons/ReelsIcon';
+import { MessengerIcon } from '@/components/icons/MessengerIcon';
 
 interface Props {
   user: User;
@@ -19,41 +15,14 @@ interface Props {
 
 export const SidebarNav = ({ user }: Props) => {
   return (
-    <nav className="border-border flex justify-evenly border-t md:flex-col md:border-0">
-      <SidebarNavItem
-        icon={<HouseIcon />}
-        iconFill={<HouseFillIcon />}
-        label={'Home'}
-        href="/"
-      />
-
+    <nav className='border-border flex justify-evenly border-t md:flex-col md:border-0'>
+      <SidebarNavItem icon={HomeIcon} label='Home' href='/' />
       <SearchButton />
-
-      <SidebarNavItem
-        icon={<ExploreIcon />}
-        iconFill={<ExploreFillIcon />}
-        label={'Explore'}
-        href={'/explore'}
-      />
-
-      <SidebarNavItem
-        icon={<ReelsIcon />}
-        iconFill={<ReelsIFillIcon />}
-        label={'Reels'}
-        href={'/reels'}
-      />
-
-      <SidebarNavItem
-        icon={<MessengerIcon />}
-        iconFill={<MessengerFillIcon />}
-        label={'Messages'}
-        href={'/messages'}
-      />
-
+      <SidebarNavItem icon={ExploreIcon} label='Explore' href='/explore' />
+      <SidebarNavItem icon={ReelsIcon} label='Reels' href='/reels' />
+      <SidebarNavItem icon={MessengerIcon} label='Messages' href='/messages' />
       <NotificationsButton />
-
       <NewPostButton />
-
       <SidebarNavItemProfile user={user} />
     </nav>
   );
--- src/features/home/sidebar/components/SidebarNavItem.tsx
+++ src/features/home/sidebar/components/SidebarNavItem.tsx
@@ -1,41 +1,38 @@
 'use client';
 
-import { ReactNode } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useSidebarStore } from '@/store/sidebar/sidebar-store';
+import { ComponentType } from 'react';
 
 interface Props {
-  icon: ReactNode;
-  iconFill: ReactNode;
+  icon: ComponentType<{ isActive: boolean }>;
   label: string;
   href: string;
 }
 
-export const SidebarNavItem = ({ icon, iconFill, label, href }: Props) => {
+export const SidebarNavItem = ({ icon: Icon, label, href }: Props) => {
   const pathname = usePathname();
   const { isSidebarCollapsed } = useSidebarStore();
 
-  return (
-    <>
-      <div className="flex h-12 items-center justify-center md:h-14 md:w-full">
-        <Link
-          className="hover:bg-background-hover flex items-center justify-start gap-4 rounded-lg p-3 md:w-full"
-          href={href}
-        >
-          {pathname === href && !isSidebarCollapsed ? iconFill : icon}
+  const isActive = pathname === href && !isSidebarCollapsed;
 
-          <span
-            className={`${
-              isSidebarCollapsed ? 'hidden' : 'xl:block'
-            } hidden leading-5 ${
-              pathname === href && !isSidebarCollapsed && 'font-bold'
-            }`}
-          >
-            {label}
-          </span>
-        </Link>
-      </div>
-    </>
+  return (
+    <div className='flex h-12 items-center justify-center md:h-14 md:w-full'>
+      <Link
+        href={href}
+        className='hover:bg-background-hover flex items-center justify-start gap-4 rounded-lg p-3 md:w-full'
+      >
+        <Icon isActive={isActive} />
+
+        <span
+          className={`${
+            isSidebarCollapsed ? 'hidden' : 'xl:block'
+          } hidden leading-5 ${isActive && !isSidebarCollapsed && 'font-bold'}`}
+        >
+          {label}
+        </span>
+      </Link>
+    </div>
   );
 };
