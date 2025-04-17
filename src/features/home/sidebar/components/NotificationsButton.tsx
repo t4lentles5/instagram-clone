c8d5065b4346ad1d2185b94a85470971956c3556
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-16 19:03:30 -0500

    add NotificationsIcon component and integrate it into NotificationsButton; remove unused NotificationsFIllIcon

--- src/features/home/sidebar/components/NotificationsButton.tsx
+++ src/features/home/sidebar/components/NotificationsButton.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { usePathname } from 'next/navigation';
-import { NotificationsFIllIcon } from '@/features/home/sidebar/icons/sidebar-nav/notifications/NotificationsFIllIcon';
-import { NotificationsIcon } from '@/features/home/sidebar/icons/sidebar-nav/notifications/NotificationsIcon';
 import { useSidebarStore } from '@/store/sidebar/sidebar-store';
+import { NotificationsIcon } from '@/components/icons/NotificationsIcon';
 
 export const NotificationsButton = () => {
   const pathname = usePathname();
@@ -36,19 +35,17 @@ export const NotificationsButton = () => {
     };
   }, [isNotificationsActive, toggleNotifications]);
 
+  const isActive = isNotificationsActive || pathname === '/notifications';
+
   return (
     <>
-      <div className="hidden h-12 items-center justify-center md:flex md:h-14 md:w-full">
+      <div className='hidden h-12 items-center justify-center md:flex md:h-14 md:w-full'>
         <button
           ref={buttonRef}
-          className="hover:bg-background-hover hidden w-full cursor-pointer items-center justify-start gap-4 rounded-lg p-3 md:flex"
+          className='hover:bg-background-hover hidden w-full cursor-pointer items-center justify-start gap-4 rounded-lg p-3 md:flex'
           onClick={toggleNotifications}
         >
-          {isNotificationsActive || pathname === `/notifications` ? (
-            <NotificationsFIllIcon />
-          ) : (
-            <NotificationsIcon />
-          )}
+          <NotificationsIcon isActive={isActive} />
           <span
             className={`${
               isSidebarCollapsed ? 'hidden' : 'xl:block'
