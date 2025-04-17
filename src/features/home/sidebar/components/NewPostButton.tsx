2d740fa5e257c019502c4edf3e5ce77ddc98014d
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-16 19:06:09 -0500

    add NewPostIcon component and update NewPostButton to use the new icon; refactor class names to use single quotes

--- src/features/home/sidebar/components/NewPostButton.tsx
+++ src/features/home/sidebar/components/NewPostButton.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useSidebarStore } from '@/store/sidebar/sidebar-store';
-import { NewPostIcon } from '@/features/home/sidebar/icons/sidebar-nav/new-post/NewPostIcon';
+import { NewPostIcon } from '@/components/icons/NewPostIcon';
 import { XIcon } from '@/features/home/sidebar/icons/XIcon';
 import { NewPostMediaIcons } from '@/features/home/sidebar/icons/NewPostMediaIcons';
 
@@ -35,9 +35,9 @@ export const NewPostButton = () => {
 
   return (
     <>
-      <div className="flex h-12 items-center justify-center md:h-14 md:w-full">
+      <div className='flex h-12 items-center justify-center md:h-14 md:w-full'>
         <button
-          className="hover:bg-background-hover flex cursor-pointer items-center justify-start gap-4 rounded-lg p-3 md:w-full"
+          className='hover:bg-background-hover flex cursor-pointer items-center justify-start gap-4 rounded-lg p-3 md:w-full'
           onClick={openNewPostDialog}
         >
           <NewPostIcon />
@@ -55,25 +55,25 @@ export const NewPostButton = () => {
       </div>
 
       {isNewPostDialogOpen && (
-        <div className="bg-opacity-80 fixed inset-0 z-50 flex h-full w-screen items-center justify-center bg-gray-900">
+        <div className='bg-opacity-80 fixed inset-0 z-50 flex h-full w-screen items-center justify-center bg-gray-900'>
           <button
             onClick={closeNewPostDialog}
-            className="absolute top-3 right-2 p-2 text-white"
+            className='absolute top-3 right-2 p-2 text-white'
           >
             <XIcon />
           </button>
           <div
             ref={dialogRef}
-            className="bg-popover relative flex h-4/6 w-10/12 flex-col rounded-lg md:h-[500px] md:w-[500px]"
+            className='bg-popover relative flex h-4/6 w-10/12 flex-col rounded-lg md:h-[500px] md:w-[500px]'
           >
-            <header className="bg-background border-separator w-full rounded-t-lg border-b p-2 text-center">
+            <header className='bg-background border-separator w-full rounded-t-lg border-b p-2 text-center'>
               Create new post
             </header>
 
-            <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+            <div className='flex h-full w-full flex-col items-center justify-center gap-4'>
               <NewPostMediaIcons />
-              <p className="text-xl">Drag photos and videos here</p>
-              <button className="hover:to-buttonPrimaryHover rounded-lg p-2 text-sm font-bold text-white">
+              <p className='text-xl'>Drag photos and videos here</p>
+              <button className='hover:to-buttonPrimaryHover rounded-lg p-2 text-sm font-bold text-white'>
                 Select from computer
               </button>
             </div>
