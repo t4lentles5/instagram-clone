c2d49728d0e8ec93a64d2cf52709752c115ae0ff
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-25 19:41:40 -0500

    fix: update UserStats component for consistent styling with text-secondary class

--- src/features/profile/components/UserStats.tsx
+++ src/features/profile/components/UserStats.tsx
@@ -1,15 +1,15 @@
 export const UserStats = () => {
   return (
     <>
-      <div className="border-border flex w-full items-center justify-evenly gap-10 border-b p-3 md:hidden">
-        <p className="text-center font-bold">
-          0<span className="block font-normal">posts</span>
+      <div className='border-border flex w-full items-center justify-evenly gap-10 border-b p-3 md:hidden'>
+        <p className='text-center font-bold'>
+          0<span className='text-secondary block font-normal'>posts</span>
         </p>
-        <button className="font-bold">
-          0 <span className="block font-normal">Followers</span>
+        <button className='font-bold'>
+          0 <span className='text-secondary block font-normal'>Followers</span>
         </button>
-        <button className="font-bold">
-          0 <span className="block font-normal">Following</span>
+        <button className='font-bold'>
+          0 <span className='text-secondary block font-normal'>Following</span>
         </button>
       </div>
     </>
