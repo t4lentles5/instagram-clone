66c3469d2d3b788c4cd1a592340ec036762ae1a4
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-29 13:12:07 -0500

    feat: add CropZoom component for zoom functionality in image cropping

---
+++ src/posts/components/CropZoom.tsx
@@ -0,0 +1,39 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import { SelectZoomIcon } from '../icons';
+
+interface Props {
+  isZoomCropOpen: boolean;
+  setIsZoomCropOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const CropZoom = ({ isZoomCropOpen, setIsZoomCropOpen }: Props) => {
+  return (
+    <>
+      <div>
+        <button
+          className={`${isZoomCropOpen ? 'bg-white text-black hover:bg-white/80' : 'bg-background-overlay hover:bg-background-overlay/80 text-white'} relative cursor-pointer rounded-full p-2 transition-colors duration-200`}
+          onClick={() => setIsZoomCropOpen(!isZoomCropOpen)}
+        >
+          <SelectZoomIcon />
+        </button>
+        {isZoomCropOpen && (
+          <div
+            className='bg-background-overlay absolute bottom-16 flex h-8 w-[132px] items-center rounded-lg px-3'
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <input
+              type='range'
+              min={0}
+              max={100}
+              defaultValue={0}
+              className='h-[2px] w-full appearance-none rounded-lg bg-black [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-none [&::-webkit-slider-thumb]:bg-white'
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
