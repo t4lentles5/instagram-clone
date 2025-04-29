237bc78f82c1828e2083cc1a54c4114f3d6b81a8
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-04-29 13:12:16 -0500

    feat: add MediaGallery component to NewPostModal for improved image selection

---
+++ src/posts/components/MediaGallery.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, SetStateAction } from 'react';
+import { DeleteIcon, OpenMediaGalleryIcon, PlusIcon } from '../icons';
+
+interface Props {
+  isMediaGalleryOpen: boolean;
+  setIsMediaGalleryOpen: Dispatch<SetStateAction<boolean>>;
+  selectedFiles: File[];
+}
+
+export const MediaGallery = ({
+  isMediaGalleryOpen,
+  setIsMediaGalleryOpen,
+  selectedFiles,
+}: Props) => {
+  return (
+    <>
+      <div>
+        <button
+          className={`${isMediaGalleryOpen ? 'bg-white text-black hover:bg-white/80' : 'bg-background-overlay hover:bg-background-overlay/80 text-white'} relative cursor-pointer rounded-full p-2 transition-colors duration-200`}
+          onClick={() => setIsMediaGalleryOpen(!isMediaGalleryOpen)}
+        >
+          <OpenMediaGalleryIcon />
+        </button>
+        {isMediaGalleryOpen && (
+          <div
+            className='bg-background-overlay absolute right-2 bottom-16 m-2 flex gap-3 rounded-lg p-2'
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            {selectedFiles.map((image) => (
+              <div
+                key={image.name}
+                className='relative max-h-[100px] max-w-[100px]'
+              >
+                <img
+                  src={URL.createObjectURL(image)}
+                  alt='Selected Image'
+                  className='aspect-square object-cover'
+                />
+                <span className='text-primary bg-background-overlay absolute top-1 right-1 rounded-full p-1'>
+                  <DeleteIcon />
+                </span>
+              </div>
+            ))}
+            <div className='text-secondary border-border grid h-12 w-12 place-items-center rounded-full border'>
+              <PlusIcon />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
--- src/posts/components/NewPostModal.tsx
+++ src/posts/components/NewPostModal.tsx
@@ -3,15 +3,11 @@ import { useEffect, useRef, useState } from 'react';
 import { useUserStore } from '@/store/user/user-store';
 
 import { NewPostMediaIcon } from '@/shared/icons';
-import {
-  BackPostIcon,
-  OpenMediaGalleryIcon,
-  PlusIcon,
-  SelectZoomIcon,
-  DeleteIcon,
-} from '@/posts/icons';
+import { BackPostIcon } from '@/posts/icons';
 import { NewPostCarousel } from './NewPostCarousel';
 import { SelectCrop } from './SelectCrop';
+import { CropZoom } from './CropZoom';
+import { MediaGallery } from './MediaGallery';
 
 interface Props {
   isOpen: boolean;
@@ -123,66 +119,17 @@ export const NewPostModal = ({ isOpen, onClose }: Props) => {
                       setIsCropOptionsOpen={setIsCropOptionsOpen}
                     />
 
-                    <div>
-                      <button
-                        className={`${isZoomCropOpen ? 'bg-white text-black hover:bg-white/80' : 'bg-background-overlay hover:bg-background-overlay/80 text-white'} relative cursor-pointer rounded-full p-2 transition-colors duration-200`}
-                        onClick={() => setIsZoomCropOpen(!isZoomCropOpen)}
-                      >
-                        <SelectZoomIcon />
-                      </button>
-                      {isZoomCropOpen && (
-                        <div
-                          className='bg-background-overlay absolute bottom-16 flex h-8 w-[132px] items-center rounded-lg px-3'
-                          onClick={(e) => {
-                            e.stopPropagation();
-                          }}
-                        >
-                          <input
-                            type='range'
-                            min={0}
-                            max={100}
-                            defaultValue={0}
-                            className='h-[2px] w-full appearance-none rounded-lg bg-black [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-none [&::-webkit-slider-thumb]:bg-white'
-                          />
-                        </div>
-                      )}
-                    </div>
+                    <CropZoom
+                      isZoomCropOpen={isZoomCropOpen}
+                      setIsZoomCropOpen={setIsZoomCropOpen}
+                    />
                   </div>
-                  <div>
-                    <button
-                      className={`${isMediaGalleryOpen ? 'bg-white text-black hover:bg-white/80' : 'bg-background-overlay hover:bg-background-overlay/80 text-white'} relative cursor-pointer rounded-full p-2 transition-colors duration-200`}
-                      onClick={() => setIsMediaGalleryOpen(!isMediaGalleryOpen)}
-                    >
-                      <OpenMediaGalleryIcon />
-                    </button>
-                    {isMediaGalleryOpen && (
-                      <div
-                        className='bg-background-overlay absolute right-2 bottom-16 m-2 flex gap-3 rounded-lg p-2'
-                        onClick={(e) => {
-                          e.stopPropagation();
-                        }}
-                      >
-                        {selectedFiles.map((image) => (
-                          <div
-                            key={image.name}
-                            className='relative max-h-[100px] max-w-[100px]'
-                          >
-                            <img
-                              src={URL.createObjectURL(image)}
-                              alt='Selected Image'
-                              className='aspect-square object-cover'
-                            />
-                            <span className='text-primary bg-background-overlay absolute top-1 right-1 rounded-full p-1'>
-                              <DeleteIcon />
-                            </span>
-                          </div>
-                        ))}
-                        <div className='text-secondary border-border grid h-12 w-12 place-items-center rounded-full border'>
-                          <PlusIcon />
-                        </div>
-                      </div>
-                    )}
-                  </div>
+
+                  <MediaGallery
+                    isMediaGalleryOpen={isMediaGalleryOpen}
+                    setIsMediaGalleryOpen={setIsMediaGalleryOpen}
+                    selectedFiles={selectedFiles}
+                  />
                 </div>
               </div>
             </>
