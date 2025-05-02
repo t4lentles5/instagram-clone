d15be2376fcebf1124a5aaf4ed0b94e180ab8a56
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-05-02 10:57:52 -0500

    style: update SignUpPage and OrComponent for consistent styling and improved readability

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
 
 export default function SignUpPage() {
   return (
-    <main className="w-[350px] py-3">
-      <div className="border-border flex w-[350px] flex-col border py-[10px]">
-        <div className="mt-9 mb-3 flex w-full items-center justify-center">
+    <main className='w-[350px] py-3'>
+      <div className='border-border flex w-[350px] flex-col border py-[10px]'>
+        <div className='mt-9 mb-3 flex w-full items-center justify-center'>
           <i
-            data-visualcompletion="css-img"
-            aria-label="Instagram"
-            role="img"
+            data-visualcompletion='css-img'
+            aria-label='Instagram'
+            role='img'
             style={{
               backgroundImage:
                 'url("https://static.cdninstagram.com/rsrc.php/v4/yB/r/E7m8ZCMOFDS.png")',
@@ -29,10 +29,10 @@ export default function SignUpPage() {
             }}
           ></i>
         </div>
-        <p className="text-secondary mx-10 mb-[10px] text-center font-semibold">
+        <p className='text-secondary mx-10 mb-[10px] text-center font-semibold'>
           Sign up to see photos and videos from your friends.
         </p>
-        <button className="text-blue mt-2 mb-5 font-semibold">
+        <button className='text-link hover:text-link-hover mt-2 mb-5 cursor-pointer font-semibold'>
           Sign in as a guest
         </button>
 
@@ -40,9 +40,12 @@ export default function SignUpPage() {
 
         <SignUpForm />
       </div>
-      <div className="border-border mt-3 w-[350px] border p-5 text-center">
-        <p className="text-sm">Have an account?</p>
-        <Link href={'/auth/login'} className="text-blue text-sm font-semibold">
+      <div className='border-border mt-3 w-[350px] border p-5 text-center'>
+        <p className='text-sm'>Have an account?</p>
+        <Link
+          href={'/auth/login'}
+          className='text-link hover:text-link-hover active:text-link/50 text-sm font-semibold'
+        >
           Log in
         </Link>
       </div>
--- src/features/auth/components/OrComponent.tsx
+++ src/features/auth/components/OrComponent.tsx
@@ -1,8 +1,8 @@
 export const OrComponent = () => {
   return (
-    <div className="px-10 py-2">
-      <div className="border-border flex h-0 w-full items-center justify-center border">
-        <p className="bg-background px-4 text-xs">OR</p>
+    <div className='w-full px-10 py-2'>
+      <div className='border-separator flex h-0 w-full items-center justify-center border'>
+        <p className='bg-background px-4 text-xs'>OR</p>
       </div>
     </div>
   );
