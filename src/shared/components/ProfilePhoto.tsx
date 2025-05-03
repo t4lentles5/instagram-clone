401879a8bbec4d79e49189a19c3887c0e84d97bb
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-05-03 16:55:56 -0500

    style: update ProfilePhoto component for improved consistency in class naming

--- src/shared/components/ProfilePhoto.tsx
+++ src/shared/components/ProfilePhoto.tsx
@@ -21,18 +21,18 @@ export const ProfilePhoto = ({
   borderDivSize,
 }: Props) => {
   return (
-    <div className="relative">
+    <div className='relative'>
       <div
         className={`absolute -top-1 -left-1 aspect-square rounded-full bg-gradient-to-tr from-amber-400 via-rose-500 to-fuchsia-600 p-0.5 ${borderDivSize.size} ${borderDivSize.mdSize}`}
       >
         <div
-          className={`bg-background aspect-square rounded-full p-0.5 ${backgroundDivSize.size} ${backgroundDivSize.mdSize}`}
+          className={`bg-ig-primary-background aspect-square rounded-full p-0.5 ${backgroundDivSize.size} ${backgroundDivSize.mdSize}`}
         />
       </div>
 
       <img
         src={profile_photo || '/default_photo.jpg'}
-        alt="Profile photo"
+        alt='Profile photo'
         className={`relative z-0 aspect-square rounded-full ${imageSize.size} ${imageSize.mdSize}`}
       />
     </div>
