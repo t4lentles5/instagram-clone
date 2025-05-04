744294bfd48a9fb5174846a824cdc69f7f43a539
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-05-04 10:17:53 -0500

    style: update Footer component for consistent text styling

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export const Footer = () => {
   return (
     <>
-      <footer className="text-secondary p-5 text-center text-xs">
+      <footer className='text-ig-tertiary-text p-5 text-center text-xs'>
         Cloned with 💜 by{' '}
         <Link
-          href="https://github.com/Obrn544"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-secondary hover:underline"
+          href='https://github.com/Obrn544'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='hover:underline'
         >
           @obrn544
         </Link>
