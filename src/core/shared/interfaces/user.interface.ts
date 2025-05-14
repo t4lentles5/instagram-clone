85338f2602c9f6b1d98b37381225d1088c8338da
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-05-13 21:28:52 -0500

    feat: update User interface to include password and follower/following counts, and remove Follow interface

--- src/core/shared/interfaces/user.interface.ts
+++ src/core/shared/interfaces/user.interface.ts
@@ -1,20 +1,15 @@
 export interface User {
   id: string;
   email: string;
+  password: string;
   fullname: string;
   username: string;
   profile_photo: string | null;
   profile_photo_id: string | null;
   bio: string | null;
-  followers: Follow[];
-  following: Follow[];
   _count: {
     posts: number;
+    followers: number;
+    following: number;
   };
-}
-
-export interface Follow {
-  id: string;
-  followerId: string;
-  followingId: string;
 }
