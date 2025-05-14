e3bffdf17ae0ac6470555e074e76fb0e670b3945
Adolfo Omar Obregon Henostroza <oobregon544@gmail.com>
2025-05-13 21:28:42 -0500

    feat: add useModal hook for managing modal state and behavior in the application

---
+++ src/core/shared/hooks/useModal.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+
+export const useModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      const originalStyles = {
+        overflow: document.body.style.overflow,
+        paddingRight: document.body.style.paddingRight,
+        position: document.body.style.position,
+        top: document.body.style.top,
+        width: document.body.style.width,
+      };
+
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      const scrollY = window.scrollY;
+
+      document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = '100%';
+
+      return () => {
+        document.body.style.overflow = originalStyles.overflow;
+        document.body.style.paddingRight = originalStyles.paddingRight;
+        document.body.style.position = originalStyles.position;
+        document.body.style.top = originalStyles.top;
+        document.body.style.width = originalStyles.width;
+
+        window.scrollTo(0, scrollY);
+      };
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (!dialog) return;
+
+    if (isOpen && !dialog.open) {
+      dialog.showModal();
+    } else if (!isOpen && dialog.open) {
+      dialog.close();
+    }
+  }, [isOpen]);
+
+  const Modal = ({ children }: { children: React.ReactNode }) => (
+    <dialog
+      ref={dialogRef}
+      className='bg-ig-elevated-background backdrop:bg-overlay-alpha-80 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-auto rounded-xl'
+      onCancel={closeModal}
+      onClick={(e) => {
+        const dialog = dialogRef.current;
+        e.stopPropagation();
+        if (dialog && e.target === dialog) closeModal();
+      }}
+    >
+      {children}
+    </dialog>
+  );
+
+  return { openModal, closeModal, isOpen, Modal };
+};
