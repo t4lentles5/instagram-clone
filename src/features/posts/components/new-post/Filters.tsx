99bf0c2fe58b96ac31516fc1777a6a57a613ded7
Omar Obregon <oobregon544@gmail.com>
2025-07-06 17:56:37 -0500

    feat: update Adjustments and Filters components for consistent styling

--- src/features/posts/components/new-post/Filters.tsx
+++ src/features/posts/components/new-post/Filters.tsx
@@ -1,1 +1,5 @@
-export const Filters = () => <div className='text-center p-4'>Coming Soon</div>;
+export const Filters = () => (
+  <div className='text-ig-primary-text flex h-full w-full items-center justify-center p-4 font-semibold'>
+    Coming Soon
+  </div>
+);
